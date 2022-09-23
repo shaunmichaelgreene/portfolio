236b4881b1a63bf09b6df8dae1dea192f21e047a
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-09-23 16:17:45 -0400

    adding portfolio data

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://shaunmichaelgreene.github.io/",
+    "name": "Shaun Greene",
+    "role": "Title Analyst and Full Stack Developer",
+    "linkedinId":"shaunmichaelgreene",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am a professional Title Analyst, Full Stack Developer, and amateur woodworker.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shaunmichaelgreene/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rbhatia46",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Shaun recently earned a Full Stack Developer Certificate by completing a 6 month instensive coding course through Michigan State University. Professionally, Shaun has worn many hats during his career; with prior experience in sales, business development, project management, oil & gas, and title research. Shaun currently works as a President's Club Title Analyst, and is focused on securing a new full-time position so that he can apply his new-found passion for web development. Shaun spends his free time building custom furniture, working on home improvements, and enjoying the outdoors with his family.",
+    "address":"Pittsburgh, PA",
+    "website":"https://https://shaunmichaelgreene.github.io",
+    "education":[
+      {
+        "UniversityName":"Michigan State University",
+        "specialization":"Full Stack Developer Certificate",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2022",
+        "Achievements":"Gradutated with a perfect score on all course projects & assignments"
+      },
+      {
+        "UniversityName":"West Virginia University",
+        "specialization":"B.S. in Business Administration (Management)",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2009",
+        "Achievements":"Personally financed 100% of cost of attendance by working full-time throughout enrollment."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Amrock, LLC",
+        "specialization":"President's Club Title Clearance Analyst",
+        "MonthOfLeaving":"Currently",
+        "YearOfLeaving":"Employed",
+        "Achievements":"Work closely with clients, team members, and 3rd parties on a day-to-day basis to analyze risk & clear title issues according to underwriter & lender requirements."
+      },
+      {
+        "CompanyName":"Larkspur Land Group, LLC",
+        "specialization":"Lease & Title Curative",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2014",
+        "Achievements":"Personally managed the disbursement/delivery of more than $20 million in lessor bonus payments."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
